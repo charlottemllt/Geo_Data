74d0fc5382b706b8425bd98d93910130c16cf3e8
Charlotte Millet <charlottemillet04@gmail.com>
2022-08-07 11:09:08 +0200

    Gestion of zip file and defining functions

--- downloads.py
+++ downloads.py
@@ -1,7 +1,24 @@
+import string
 import requests
+import gzip
+import shutil
 import config
 
-dataset_url = config.DATASET_ADRESSES_URL
-r = requests.get(dataset_url, stream=True)
-with open("adresses-france.csv.gz",'wb') as f:
-    f.write(r.content)
+
+def download_file(file_url: string, filename: string) -> None:
+    r = requests.get(file_url, stream=True)
+    with open(filename,'wb') as f:
+        f.write(r.content)
+
+def convert_gz_to_csv(gz_filename: string, csv_filename: string) -> None:
+    with gzip.open(gz_filename, 'rb') as f_in:
+        with open(csv_filename, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+if __name__ == '__main__':
+    dataset_url = config.DATASET_ADRESSES_URL
+    gz_filename = 'adresses-france.csv.gz'
+    csv_filename = 'adresses-france.csv'
+
+    download_file(dataset_url, gz_filename)
+    convert_gz_to_csv(gz_filename, csv_filename)
